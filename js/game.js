4fe9f515e6c199497ba49937ea975f1a56305e6a
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-28 23:48:38 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -171,6 +171,9 @@ checkWindow();
 
 window.addEventListener('resize', () => {
 
+    let vh = window.innerHeight * 0.01;
+    document.body.style.setProperty('--vh', `${vh}px`);
+
     if(window.innerWidth < window.innerHeight){
         refs.gameBox.classList.remove("gamebox--vmin"); 
         refs.gameBox.classList.add("gamebox--vmax");   
