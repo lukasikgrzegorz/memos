8969d0e60c8b1283a8843622c275aed49354da48
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-28 08:26:37 +0200

    create refs object
    add newGame method

--- js/game.js
+++ js/game.js
@@ -1,152 +1,136 @@
-let cardsNumber = 24;
-let moveCounter = 0;
-let cardTemp = [];
-let statusTemp = {};
-
-
-if (localStorage.getItem("gameStatus")) {
-    statusTemp = JSON.parse(localStorage.getItem("gameStatus"));
-}
-
-const gameBox = document.querySelector("[js-gamebox]");
-
-const randomColor = () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
-
-const createColorPair = () => {
-
-    let newArray = [];
-
-    for (i = 0; i < cardsNumber / 2; i++) {
-        newArray.push(randomColor());
-    }
-
-    return newArray;
-}
-
-const shuffleColors = (array) => {
-    let newArray = array.sort(() => Math.random() - .5)
+const refs = {
+    cardsNumber : 16,
+    moveCounter : 0,
+    cardTemp : [],
+    statusTemp : {},
+    gameBox: document.querySelector("[js-gamebox]"),
     
-    return newArray;
-}
+    randomColor(){
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    },
 
-const createCards = (array) => {
-    
-    for (i = 0; i < cardsNumber; i++){
-        const newCard = document.createElement("div");
-        newCard.classList.add("card");
-        newCard.id = `card-${i}`;
-        newCard.setAttribute("data-color", array[i]);
-
-        statusTemp[`${newCard.id}`] = { color: array[i], status: "back" };
-       
-        const newCardWrapper = document.createElement("div");
-        newCardWrapper.classList.add("card__wrapper");
-        
-        const newCardBack = document.createElement("div");
-        newCardBack.classList.add("card__back");
-        
-        const newCardFront = document.createElement("div");
-        newCardFront.classList.add("card__front");
-        newCardFront.style.backgroundColor = array[i];
-        
-        newCardWrapper.append(newCardBack, newCardFront);
-        newCard.append(newCardWrapper);
-        gameBox.append(newCard);
-    }
-}
-
-const checkWin = () => {
-    let array = [];
-    
-    for (let key in statusTemp) {
-        array.push(statusTemp[key].status);
-    }
-
-    if (array.every(e => e === "clear")) {
-        localStorage.clear();    
-        setTimeout(()=>window.location.reload(),1000)
-    }
-}
-
-//Local storage read
-
-const statusTempUpdate = () => {
-    localStorage.setItem("gameStatus", JSON.stringify(statusTemp));     
-}
-
-const statusTempState = () => {
-    
-    for (i = 0; i < cardsNumber; i++){
-        const newCard = document.createElement("div");
-        newCard.classList.add("card");
-        newCard.id = `card-${i}`;
-        newCard.setAttribute("data-color", statusTemp[`card-${i}`].color);
-
-        const newCardWrapper = document.createElement("div");
-        newCardWrapper.classList.add("card__wrapper");
-        
-        const newCardBack = document.createElement("div");
-        newCardBack.classList.add("card__back");
-        
-        const newCardFront = document.createElement("div");
-        newCardFront.classList.add("card__front");
-        newCardFront.style.backgroundColor = statusTemp[`card-${i}`].color;
-
-        if (statusTemp[`card-${i}`].status === "clear") {
-            newCard.classList.add("no-visible");    
+    createColorPair(){
+        let newArray = [];
+        for (i = 0; i < this.cardsNumber / 2; i++) {
+            newArray.push(this.randomColor());
         }
-        
-        newCardWrapper.append(newCardBack, newCardFront);
-        newCard.append(newCardWrapper);
-        gameBox.append(newCard);
-        
+        return newArray;
+    },
+
+    shuffleColors(array){
+        let newArray = array.sort(() => Math.random() - .5)
+        return newArray;
+    },
+
+    createCards(array){  
+        for (i = 0; i < this.cardsNumber; i++){
+            const newCard = document.createElement("div");
+            newCard.classList.add("card");
+            newCard.id = `card-${i}`;
+            newCard.setAttribute("data-color", array[i]);
+            this.statusTemp[`${newCard.id}`] = { color: array[i], status: "back" };
+            const newCardWrapper = document.createElement("div");
+            newCardWrapper.classList.add("card__wrapper");
+            const newCardBack = document.createElement("div");
+            newCardBack.classList.add("card__back");
+            const newCardFront = document.createElement("div");
+            newCardFront.classList.add("card__front");
+            newCardFront.style.backgroundColor = array[i];
+            newCardWrapper.append(newCardBack, newCardFront);
+            newCard.append(newCardWrapper);
+            this.gameBox.append(newCard);
+        }
+    },
+
+    checkWin(){
+        let array = [];
+        for (let key in this.statusTemp) {
+            array.push(this.statusTemp[key].status);
+        }
+        if (array.every(e => e === "clear")) {
+            localStorage.clear();    
+            setTimeout(()=>window.location.reload(),1000)
+        }
+    },
+
+    checkLocalTemp(){
+        if (localStorage.getItem("gameStatus")) {
+            this.statusTemp = JSON.parse(localStorage.getItem("gameStatus"));
+        }
+    },
+
+    tempUpdate(){
+        localStorage.setItem("gameStatus", JSON.stringify(this.statusTemp));     
+    },
+
+    createCardsFromTemp(){ 
+        for (i = 0; i < this.cardsNumber; i++){
+            const newCard = document.createElement("div");
+            newCard.classList.add("card");
+            newCard.id = `card-${i}`;
+            newCard.setAttribute("data-color", this.statusTemp[`card-${i}`].color);
+            const newCardWrapper = document.createElement("div");
+            newCardWrapper.classList.add("card__wrapper");   
+            const newCardBack = document.createElement("div");
+            newCardBack.classList.add("card__back");
+            const newCardFront = document.createElement("div");
+            newCardFront.classList.add("card__front");
+            newCardFront.style.backgroundColor = this.statusTemp[`card-${i}`].color;
+            
+            if (this.statusTemp[`card-${i}`].status === "clear") {
+                newCard.classList.add("no-visible");    
+            }
+
+            newCardWrapper.append(newCardBack, newCardFront);
+            newCard.append(newCardWrapper);
+            this.gameBox.append(newCard);
+        }
+    },  
+    
+    nweGame() {
+        this.checkLocalTemp();
+        if (!localStorage.getItem("gameStatus")) {
+            let colorPair = this.createColorPair();
+            colorPair = colorPair.concat(colorPair);
+            let colorPairShuffled = this.shuffleColors(colorPair);
+            this.createCards(colorPairShuffled);
+        } else {
+            this.createCardsFromTemp();
+        } 
     }
 }
 
-//New Game
-
-if (!localStorage.getItem("gameStatus")) {
-    let colorPair = createColorPair();
-    colorPair = colorPair.concat(colorPair);
-    colorPairShuffled = shuffleColors(colorPair);
-    createCards(colorPairShuffled);
-} else {
-    statusTempState();
-} 
-
-
+refs.nweGame();
 
 //Main Event Listener
 
-gameBox.addEventListener("click", (e) => {
+refs.gameBox.addEventListener("click", (e) => {
     
-    if (e.target.classList.contains("card") && cardTemp.length<2){
+    if (e.target.classList.contains("card") && refs.cardTemp.length<2){
         
         e.target.classList.toggle("flip");
         e.target.firstElementChild.classList.toggle("flip"); 
-        cardTemp.push({id: `#${e.target.id}`, bgCol: e.target.getAttribute("data-color")});
-        moveCounter++;
-        statusTemp[`${e.target.id}`].status = "front";
-        statusTempUpdate();
+        refs.cardTemp.push({id: `#${e.target.id}`, bgCol: e.target.getAttribute("data-color")});
+        refs.moveCounter++;
+        refs.statusTemp[`${e.target.id}`].status = "front";
+        refs.tempUpdate();
 
         
-        if (moveCounter === 2) {
+        if (refs.moveCounter === 2) {
             
-            item1 = document.querySelector(cardTemp[0].id);
-            item2 = document.querySelector(cardTemp[1].id);
+            item1 = document.querySelector(refs.cardTemp[0].id);
+            item2 = document.querySelector(refs.cardTemp[1].id);
            
-            if (cardTemp[0].bgCol === cardTemp[1].bgCol && cardTemp[0].id !== cardTemp[1].id ) {   
+            if (refs.cardTemp[0].bgCol === refs.cardTemp[1].bgCol && refs.cardTemp[0].id !== refs.cardTemp[1].id ) {   
                 setTimeout(()=>{
                     item1.classList.add("no-visible");
                     item2.classList.add("no-visible");
-                    statusTemp[`${item1.id}`].status = "clear";
-                    statusTemp[`${item2.id}`].status = "clear";
-                    statusTempUpdate();
-                    moveCounter = 0;
-                    cardTemp = [];
-                    checkWin();
+                    refs.statusTemp[`${item1.id}`].status = "clear";
+                    refs.statusTemp[`${item2.id}`].status = "clear";
+                    refs.tempUpdate();
+                    refs.moveCounter = 0;
+                    refs.cardTemp = [];
+                    refs.checkWin();
                 },1200)
             } else {
                 setTimeout(()=>{
@@ -154,11 +138,11 @@ gameBox.addEventListener("click", (e) => {
                     item1.firstElementChild.classList.toggle("flip");
                     item2.classList.toggle("flip");
                     item2.firstElementChild.classList.toggle("flip");
-                    statusTemp[`${item1.id}`].status = "back";
-                    statusTemp[`${item2.id}`].status = "back";
-                    statusTempUpdate();
-                    moveCounter = 0;
-                    cardTemp = [];
+                    refs.statusTemp[`${item1.id}`].status = "back";
+                    refs.statusTemp[`${item2.id}`].status = "back";
+                    refs.tempUpdate();
+                    refs.moveCounter = 0;
+                    refs.cardTemp = [];
                 },1200)
                
             }
