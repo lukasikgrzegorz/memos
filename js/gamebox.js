cfef099333f4db6a47fdf0a2fdc92867c7ad037c
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-31 23:00:28 +0100

    add fast click possibilitly

--- js/gamebox.js
+++ js/gamebox.js
@@ -1,21 +1,25 @@
 const result = {
-    hit(el1,el2){
-        el1.classList.add("no-visible");
-        el2.classList.add("no-visible");
+    hit(el1,el2,t){
         engine.stateTemp[`${item1.id}`].status = "clear";
         engine.stateTemp[`${item2.id}`].status = "clear";
+        setTimeout(() => {
+        el1.classList.add("no-visible");
+        el2.classList.add("no-visible");
+        }, t)
         engine.tempUpdate();
         engine.moveCounter = 0;
         engine.chosenCard = [];
         engine.checkWin();
     },
-    miss(el1,el2) {
-        el1.classList.toggle("flip");
-        el1.firstElementChild.classList.toggle("flip");
-        el2.classList.toggle("flip");
-        el2.firstElementChild.classList.toggle("flip");
+    miss(el1,el2,t) {
         engine.stateTemp[`${item1.id}`].status = "back";
-        engine.stateTemp[`${item2.id}`].status = "back";
+        engine.stateTemp[`${item2.id}`].status = "back";  
+        setTimeout(() => {
+            el1.classList.toggle("flip");
+            el1.firstElementChild.classList.toggle("flip");
+            el2.classList.toggle("flip");
+            el2.firstElementChild.classList.toggle("flip");
+        },t)
         engine.tempUpdate();
         engine.moveCounter = 0;
         engine.chosenCard = [];    
@@ -36,9 +40,9 @@ engine.gameBox.addEventListener("click", (e) => {
             item1 = document.querySelector(engine.chosenCard[0].id);
             item2 = document.querySelector(engine.chosenCard[1].id);
             if (engine.chosenCard[0].bgCol === engine.chosenCard[1].bgCol && engine.chosenCard[0].id !== engine.chosenCard[1].id) {
-                setTimeout(()=>result.hit(item1,item2), 1200)
+                result.hit(item1, item2, 1200);
             } else {
-                setTimeout(()=>result.miss(item1,item2), 1200)  
+                result.miss(item1, item2, 1200);
             }
         }
     }
